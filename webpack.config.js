eb389bdbc5022c02760dc97e831d699aca549da4
Daniel <dpip.nc@gmail.com>
2017-09-13 16:24:45 -0400

    really appretiate create react app // fought with webpack for an hour until i found a stack solution // needs styling

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+          loaders: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react', 'stage-0']
+          }
+        }
+      ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+    }
+};
